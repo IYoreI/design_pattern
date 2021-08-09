58a5e1511fc26888d8ac1b10851aeb69bd22c888
yore <dy_yore@126.com>
2021-08-09 15:20:51 +0800

    init project & add singleton mode code

---
+++ src/main/java/mode/singleton/SingletonEnum.java
@@ -0,0 +1,30 @@
+package mode.singleton;
+
+/**
+ * @author Yore
+ * @date 2021/8/3 21:01
+ * @description 枚举类实现单例模式
+ */
+public class SingletonEnum {
+}
+//获取单例的方式：
+//SingletonInstance.INSTANCE.getInstance()
+
+enum SingletonInstance {
+    /**
+     * 引用
+     */
+    INSTANCE;
+    /**
+     * 单例
+     */
+    private final SingletonEnum instance;
+
+    SingletonInstance() {
+        instance = new SingletonEnum();
+    }
+
+    public SingletonEnum getInstance() {
+        return instance;
+    }
+}

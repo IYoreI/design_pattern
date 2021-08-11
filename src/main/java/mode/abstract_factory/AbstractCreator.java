2a9048537bfc4b892fc212d2c6874efc657b15ce
yore <dy_yore@126.com>
2021-08-11 11:18:35 +0800

    init abstract factory mode

---
+++ src/main/java/mode/abstract_factory/AbstractCreator.java
@@ -0,0 +1,13 @@
+package mode.abstract_factory;
+
+/**
+ * @author Yore
+ * @date 2021/8/11 11:13
+ * @description
+ */
+public abstract class AbstractCreator {
+    //构建A产品家族
+    public abstract AbstractProductA createProductA();
+    //构建B产品家族
+    public abstract AbstractProductB createProductB();
+}
---
+++ src/main/java/mode/abstract_factory/ProductB2.java
@@ -0,0 +1,13 @@
+package mode.abstract_factory;
+
+/**
+ * @author Yore
+ * @date 2021/8/11 11:12
+ * @description
+ */
+public class ProductB2 extends AbstractProductB {
+    @Override
+    public void doSomething() {
+        System.out.println("产品B2的实现方法");
+    }
+}

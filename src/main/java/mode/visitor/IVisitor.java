5be551bc36aeff8899c581a027543053df961b1c
yore <dy_yore@126.com>
2021-08-18 17:03:52 +0800

    init visitor mode & update doc

---
+++ src/main/java/mode/visitor/IVisitor.java
@@ -0,0 +1,11 @@
+package mode.visitor;
+
+/**
+ * @author Yore
+ * @date 2021/8/18 16:48
+ * @description 抽象访问者
+ */
+public interface IVisitor {
+    public void visit(ConcreteElement1 el1);
+    public void visit(ConcreteElement2 el2);
+}
---
+++ src/main/java/mode/visitor/Visitor.java
@@ -0,0 +1,17 @@
+package mode.visitor;
+
+/**
+ * @author Yore
+ * @date 2021/8/18 16:51
+ * @description
+ */
+public class Visitor implements IVisitor{
+
+    public void visit(ConcreteElement1 el1) {
+        el1.doSomething();
+    }
+
+    public void visit(ConcreteElement2 el2) {
+        el2.doSomething();
+    }
+}
